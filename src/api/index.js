23c34d30e1c10eeeb5d02dbad00dd8c750e4e512
Richard Lindsay <richard@tictocfamily.com>
2020-07-05 17:12:43 +0100

    Initial commit 🦄

---
+++ src/api/index.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const axios = require('axios');
+const e = require('express');
+
+const router = express.Router();
+
+const BASE_URL = 'https://www.footballwebpages.co.uk/fixtures-results.json?';
+
+router.get('/', async(req, res, next) => {
+  try {
+    const PARAMS = new URLSearchParams({
+      team: '343',
+      results: '1',
+      fixtures: '0'
+    });
+
+    let { data } = await axios.get(`${BASE_URL}${PARAMS}`);
+
+    let homeTeamNumber = data.matchesTeam.match[0].homeTeamNo;
+    let homeTeamScore = data.matchesTeam.match[0].homeTeamScore;
+    let awayTeamScore = data.matchesTeam.match[0].awayTeamScore;
+
+    if (homeTeamNumber == 343) {
+      if (homeTeamScore > awayTeamScore) {
+        res.json({
+          message: 'Yes'
+        });
+      } else {
+        res.json({
+          message: 'No'
+        });
+      }
+    } else {
+      if (awayTeamScore > homeTeamScore) {
+        res.json({
+          message: 'Yes'
+        });
+      } else {
+        res.json({
+          message: 'No'
+        });
+      }
+    }
+  } catch (error) {
+    next(error);
+  }
+
+});
+
+module.exports = router;
